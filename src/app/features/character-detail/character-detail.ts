f24c6809b4f95d43008c5b5852eaa8891216a77a
Alex Kozii <alex.kozii93@gmail.com>
2026-01-16 21:25:15 +0200

    add detail-skeleton

--- src/app/features/character-detail/character-detail.ts
+++ src/app/features/character-detail/character-detail.ts
@@ -2,10 +2,11 @@ import { Component, inject, Input, input, signal } from '@angular/core';
 import { CharacterService } from '../../core/services/character';
 import { Character } from '../../core/models/character.model';
 import { RouterLink } from '@angular/router';
+import { DetailSkeleton } from './detail-skeleton/detail-skeleton';
 
 @Component({
   selector: 'app-character-detail',
-  imports: [RouterLink],
+  imports: [RouterLink, DetailSkeleton],
   templateUrl: './character-detail.html',
   styleUrl: './character-detail.scss',
 })
---
+++ src/app/features/character-detail/detail-skeleton/detail-skeleton.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-skeleton',
+  imports: [],
+  templateUrl: './detail-skeleton.html',
+  styleUrl: './detail-skeleton.scss',
+})
+export class DetailSkeleton {
+
+}
