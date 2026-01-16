023b4ede864119edc645435f238f25b950fc20ea
Alex Kozii <alex.kozii93@gmail.com>
2026-01-16 22:46:49 +0200

    refactor character-service

--- src/app/core/services/character.ts
+++ src/app/core/services/character.ts
@@ -17,21 +17,18 @@ export class CharacterService {
   errorMessage = signal<string | null>(null);
   totalInfo = signal<{ pages: number; count: number } | null>(null);
 
-  updateSearchName(name: string) {
-    this.searchTerm.set(name);
-    this.currentPage.set(1);
-  }
+  private searchTerm$ = toObservable(this.searchTerm);
+  private currentPage$ = toObservable(this.currentPage);
 
   characters = toSignal(
-    combineLatest([toObservable(this.searchTerm), toObservable(this.currentPage)]).pipe(
+    combineLatest([this.searchTerm$, this.currentPage$]).pipe(
       debounceTime(300),
-
-      tap(([name, page]) => {
+      tap(() => {
         this.isLoading.set(true);
         this.errorMessage.set(null);
       }),
-      switchMap(([name, page]) => {
-        return this.http.get<CharacterResponse>(`${this.apiUrl}/?name=${name}&page=${page}`).pipe(
+      switchMap(([name, page]) =>
+        this.http.get<CharacterResponse>(`${this.apiUrl}/?name=${name}&page=${page}`).pipe(
           tap((res) => {
             this.totalInfo.set(res.info);
             this.isLoading.set(false);
@@ -40,16 +37,27 @@ export class CharacterService {
           catchError((error) => {
             this.isLoading.set(false);
             this.totalInfo.set(null);
+
             if (error.status === 404) {
-              this.errorMessage.set(`Nothing found`);
+              this.errorMessage.set('Nothing found');
+            } else {
+              this.errorMessage.set('Something went wrong. Please try again later.');
             }
+
             return of([]);
           }),
-        );
-      }),
+        ),
+      ),
     ),
     { initialValue: [] as Character[] },
   );
+
+  updateSearchName(name: string) {
+    if (this.searchTerm() === name) return;
+
+    this.searchTerm.set(name);
+    this.currentPage.set(1);
+  }
 
   getCharacterById(id: string) {
     return this.http.get<Character>(`${this.apiUrl}/${id}`);
